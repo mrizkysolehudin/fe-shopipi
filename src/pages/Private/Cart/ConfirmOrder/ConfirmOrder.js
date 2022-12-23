bcdc54154ff1ca41a69585531d1de3e85f903c49
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2022-12-23 19:24:16 +0700

    finished pages/private/cart/confirmOrder

--- src/pages/Private/Cart/ConfirmOrder/ConfirmOrder.js
+++ src/pages/Private/Cart/ConfirmOrder/ConfirmOrder.js
@@ -3,9 +3,37 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import CheckoutSteps from "../CheckoutSteps/CheckoutSteps";
 import "./ConfirmOrder.css";
-import usb from "assets/usb.jpg";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const ConfirmOrder = () => {
+	const navigate = useNavigate();
+
+	const auth = useSelector((state) => state.auth);
+	const { user } = auth;
+	const cart = useSelector((state) => state.cart);
+	const { cartItems, shippingInfo } = cart;
+
+	const subTotalPrice = cartItems.reduce(
+		(acc, item) => acc + item.price * item.quantity,
+		0
+	);
+	const shippingPrice = subTotalPrice > 200 ? 0 : 25;
+	const taxPrice = Number((0.05 * subTotalPrice).toFixed(2));
+	const totalPrice = (subTotalPrice + shippingPrice + taxPrice).toFixed(2);
+
+	const processToPayment = () => {
+		const data = {
+			subTotalPrice: subTotalPrice.toFixed(2),
+			shippingPrice,
+			taxPrice,
+			totalPrice,
+		};
+
+		sessionStorage.setItem("orderInfo", JSON.stringify(data));
+		navigate("/payment");
+	};
+
 	return (
 		<div>
 			<MetaData title="Confim Order" />
@@ -20,15 +48,20 @@ const ConfirmOrder = () => {
 							<div className="ms-4 mt-4">
 								<p>
 									<span className="fw-bolder">Name:</span>{" "}
-									zeus nolimit
+									{user?.name}
 								</p>
 								<p>
 									<span className="fw-bolder">Phone:</span>{" "}
-									324432
+									{shippingInfo?.phoneNo}
 								</p>
-								<p>
-									<span className="fw-bolder">Address:</span>{" "}
-									New York, South, 3433, USA
+								<p className="d-flex">
+									<span className="fw-bolder me-2">
+										Address:
+									</span>{" "}
+									{shippingInfo?.address},{" "}
+									{shippingInfo?.city},{" "}
+									{shippingInfo?.postalCode},{" "}
+									{shippingInfo?.country}
 								</p>
 							</div>
 						</section>
@@ -37,33 +70,28 @@ const ConfirmOrder = () => {
 							<h3>Your Cart Items:</h3>
 
 							<div className="d-grid mt-4 gap-4">
-								<article className="card-product">
-									<img src={usb} alt="" />
+								{cartItems?.map((cartItem, index) => (
+									<article
+										key={index}
+										className="card-product">
+										<img src={cartItem?.image} alt="" />
 
-									<p className="col-6 title">
-										SanDisk Ultra 128GB SDXC UHS-I Memory
-										Card up to 80MB/s
-									</p>
+										<p className="col-6 title">
+											{cartItem?.name}
+										</p>
 
-									<p className="col-auto price">
-										11 x $11 ={" "}
-										<span className="fw-bold">$121.00</span>
-									</p>
-								</article>
-
-								<article className="card-product">
-									<img src={usb} alt="" />
-
-									<p className="col-6 title">
-										SanDisk Ultra 128GB SDXC UHS-I Memory
-										Card up to 80MB/s
-									</p>
-
-									<p className="col-auto price">
-										11 x $11 ={" "}
-										<span className="fw-bold">$121.00</span>
-									</p>
-								</article>
+										<p className="col-auto price">
+											{cartItem?.quantity} x $
+											{cartItem?.price} ={" "}
+											<span className="fw-bold">
+												{(
+													cartItem.quantity *
+													cartItem.price
+												).toFixed(2)}
+											</span>
+										</p>
+									</article>
+								))}
 							</div>
 						</section>
 					</div>
@@ -74,23 +102,29 @@ const ConfirmOrder = () => {
 						<section>
 							<div className="d-flex justify-content-between">
 								<p>Subtotal:</p>
-								<span className="fw-bolder">$605</span>
+								<span className="fw-bolder">
+									${subTotalPrice.toFixed(2)}
+								</span>
 							</div>
 							<div className="d-flex justify-content-between">
 								<p>Shipping:</p>
-								<span className="fw-bolder">$605</span>
+								<span className="fw-bolder">
+									${shippingPrice}
+								</span>
 							</div>
 							<div className="d-flex justify-content-between">
 								<p>Tax:</p>
-								<span className="fw-bolder">$605</span>
+								<span className="fw-bolder">${taxPrice}</span>
 							</div>
 
 							<div className="d-flex justify-content-between total">
 								<p>Total:</p>
-								<span className="fw-bolder">$685</span>
+								<span className="fw-bolder">${totalPrice}</span>
 							</div>
 
-							<button className="btn-orange btn-proceed">
+							<button
+								onClick={processToPayment}
+								className="btn-orange btn-proceed">
 								Proceed to Payment
 							</button>
 						</section>
